5de8792cb494645757d4caf3a46b2cf0a68f51c9
jorgesancere <jorsancere@gmail.com>
2026-02-20 13:53:44 +0100

    Empezada la clase jugador, con init y atributos de instancia

--- Esports.py
+++ Esports.py
@@ -1,2 +1,17 @@
 #Trabajo Esports.py
+#Vamos a crear la clase jugador en la que le vamos a meter el nick y otros datos:
+
+class Jugador:
+    def __init__(self, nick, nombre, apellidos, edad, pais):
+        self.nick = nick
+        self.nombre = nombre
+        self.apellidos = apellidos
+        self.edad = edad
+        self.pais = pais
+        self.victorias = 0
+        self.derrotas = 0
+
+
+
+
 
