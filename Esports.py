fef94b5e4cdfbeb9dee20b10f1f2fe5b844a1e0a
jorgesancere <jorsancere@gmail.com>
2026-02-20 14:34:13 +0100

    Clases juego y equipo creadas

--- Esports.py
+++ Esports.py
@@ -2,6 +2,8 @@
 #Vamos a crear la clase jugador en la que le vamos a meter el nick y otros datos:
 
 class Jugador:
+    #Atributos de clase:
+    cont_jug = 0 #Contador para ver cuantos jugadores tenemos
     #Atributos de instancia
     def __init__(self, nick, nombre, apellidos, edad, pais):
         self.nick = nick
@@ -11,6 +13,8 @@ class Jugador:
         self.pais = pais
         self.victorias = 0
         self.derrotas = 0
+        type(self).cont_jug +=1
+        self.codigo_jug = type(self).cont_jug
 
     #Metodo winrate(proporción victorias-derrotas)
     def winrate(self):
@@ -29,3 +33,24 @@ class Jugador:
 
         #Print de la ficha competitiva
         print(f'EL jugador {self.nick}, con un winrate del {wr}% se le considera un {self.estatus}')
+
+#Clase videojuego
+class Juego:
+    def __init__(self, nombre, cod, genero):
+        self.nombre = nombre
+        self.cod = cod
+        self.genero = genero
+
+
+#Clase equipo eSports
+class Equipo:
+    #Atributos de clase:
+    cont_equipos = 0  # Contador para ver cuantos equipos tenemos
+    def __init__(self, nombre, region, ranking, juegos):
+        self.nombre = nombre
+        self.region = region
+        self.ranking = ranking
+        self.juegos = juegos #VA A SER UNA LISTA
+        #Para el contador de equipos
+        type(self).cont_equipos +=1
+        self.codigo_equipo = type(self).cont_equipos
