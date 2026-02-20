12c842538d5bb9a43815d93a0df779c4f5d77bd8
Javier R <javierrs007@gmail.com>
2026-02-20 14:18:20 +0100

    Añadido metodo winrate()  y metodo ficha_competitiva()

--- Esports.py
+++ Esports.py
@@ -1,7 +1,8 @@
-#Trabajo Esports.py
+#Trabajo eSports.py
 #Vamos a crear la clase jugador en la que le vamos a meter el nick y otros datos:
 
 class Jugador:
+    #Atributos de instancia
     def __init__(self, nick, nombre, apellidos, edad, pais):
         self.nick = nick
         self.nombre = nombre
@@ -11,7 +12,20 @@ class Jugador:
         self.victorias = 0
         self.derrotas = 0
 
+    #Metodo winrate(proporción victorias-derrotas)
+    def winrate(self):
+        if self.derrotas==0:    #Evitamos dividir entre 0
+            return 100
+        else:
+            return (self.victorias/self.derrotas)*100
 
+    #Muestra el winrate
+    def ficha_competitiva(self):
+        wr=self.winrate()
+        if wr>70:
+            self.estatus='favorito'
+        else:
+            self.estatus='Don Nadie'
 
-
-
+        #Print de la ficha competitiva
+        print(f'EL jugador {self.nick}, con un winrate del {wr}% se le considera un {self.estatus}')
