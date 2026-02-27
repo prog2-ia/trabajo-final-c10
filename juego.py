82c934003f614e9262b51df04ed17556f1483442
Jorge <jorsancere@gmail.com>
2026-02-27 13:20:50 +0100

    Fichero clase juego

---
+++ juego.py
@@ -0,0 +1,8 @@
+#Trabajo eSports.py
+
+#Clase videojuego
+class Juego:
+    def __init__(self, nombre, cod, genero):
+        self.nombre = nombre
+        self.cod = cod
+        self.genero = genero
