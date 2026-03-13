0b51536cc4eac95c69dae4b831d496b50c300009
Jorge <jorsancere@gmail.com>
2026-03-13 14:32:32 +0100

    Clase Partida y mejoras en clase jugador

--- jugador.py
+++ jugador.py
@@ -21,11 +21,12 @@ class Jugador:
         if self.__derrotas==0:    #Evitamos dividir entre 0
             return 100
         else:
-            return (self.__victorias/self.__derrotas + self.__victorias)*100
+            return (self.__victorias/(self.__derrotas + self.__victorias))*100
 
     #Muestra el winrate
     def ficha_competitiva(self):
         wr=self.winrate()
+        #Queremos que el WR influya en las probabilidades de ganar de cada equipo
         if wr>70:
             self.__estatus='favorito'
         else:
