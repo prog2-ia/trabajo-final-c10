2f267c94a42586b14012fe39bccc4935a4ddf02d
jorgesancere <jorge.san.cere@gmail.com>
2026-05-17 23:31:54 +0200

    Mejora logica eliminaciones (eliminacion directa y doble)

--- Eliminaciones/dobleeliminacion.py
+++ Eliminaciones/dobleeliminacion.py
@@ -1,16 +1,18 @@
-#Doble eliminación(si te eliminas hay 'repesca')
+# Doble eliminacion: un equipo necesita perder dos veces para quedar eliminado
 
 from Eliminaciones.bracketeliminacion import BracketEliminacion
 from Principal.fase import Fase
 from Principal.partida import Partida
 
+
 class DobleEliminacion(BracketEliminacion):
     def __init__(self, equipos):
         super().__init__(equipos)
-        self._bracket_ganadores = []
-        self._bracket_perdedores = []
+        self._bracket_ganadores = []   # fases del bracket superior
+        self._bracket_perdedores = []  # fases del bracket inferior
 
     def gen_bracket(self):
+        # Generamos la primera fase del bracket de ganadores
         equipos = self._equipos.copy()
         fase_g = Fase(1)
 
@@ -22,24 +24,60 @@ class DobleEliminacion(BracketEliminacion):
         self._fases.append(fase_g)
 
     def procesar_fase(self):
-        fase_actual = self._bracket_ganadores[-1]
-        nueva_fase_g = Fase(fase_actual.numero + 1)
-        nueva_fase_p = Fase(fase_actual.numero)
+        # Recoge los resultados de la última fase del bracket de ganadores,
+        # manda a los ganadores a la siguiente fase del bracket de ganadores,
+        # y a los perdedores al bracket de perdedores.
 
-        for partida in fase_actual.partidas:
-            ganador = partida.resultado.ganador()
-            if ganador == 1:
-                ganador_eq = partida.equipo1
-                perdedor_eq = partida.equipo2
+        fase_g_actual = self._bracket_ganadores[-1]
+        ganadores = []
+        nuevos_perdedores = []
+
+        for partida in fase_g_actual.partidas:
+            ganador = partida.ganador()
+            if ganador == partida.equipo1:
+                ganadores.append(partida.equipo1)
+                nuevos_perdedores.append(partida.equipo2)
             else:
-                ganador_eq = partida.equipo2
-                perdedor_eq = partida.equipo1
+                ganadores.append(partida.equipo2)
+                nuevos_perdedores.append(partida.equipo1)
 
-            nueva_fase_g.partidas.append(Partida(ganador_eq, None))
-            nueva_fase_p.partidas.append(Partida(perdedor_eq, None))
+        # Procesamos también el bracket de perdedores si ya tiene fases
+        supervivientes_perdedores = []
+        if self._bracket_perdedores:
+            fase_p_actual = self._bracket_perdedores[-1]
+            for partida in fase_p_actual.partidas:
+                # El que gana aqui sigue vivo, el que pierde queda eliminado
+                ganador = partida.ganador()
+                if ganador == partida.equipo1:
+                    supervivientes_perdedores.append(partida.equipo1)
+                else:
+                    supervivientes_perdedores.append(partida.equipo2)
 
-        self._bracket_ganadores.append(nueva_fase_g)
-        self._bracket_perdedores.append(nueva_fase_p)
-        self._fases.append(nueva_fase_g)
+        # Comprobamos si estamos en la gran final
+        # (1 ganador en bracket de ganadores + 1 superviviente en bracket de perdedores)
+        if len(ganadores) == 1 and len(supervivientes_perdedores) == 1:
+            gran_final = Fase(fase_g_actual.numero + 1)
+            gran_final.partidas.append(Partida(ganadores[0], supervivientes_perdedores[0]))
+            self._fases.append(gran_final)
+            return
 
+        # Generamos la siguiente fase del bracket de ganadores
+        if len(ganadores) > 1:
+            nueva_fase_g = Fase(fase_g_actual.numero + 1)
+            for i in range(0, len(ganadores), 2):
+                if i + 1 < len(ganadores):
+                    nueva_fase_g.partidas.append(Partida(ganadores[i], ganadores[i + 1]))
+            self._bracket_ganadores.append(nueva_fase_g)
+            self._fases.append(nueva_fase_g)
+
+        # Generamos la siguiente fase del bracket de perdedores
+        # mezclando supervivientes anteriores con los nuevos perdedores del bracket de ganadores
+        todos_perdedores = supervivientes_perdedores + nuevos_perdedores
+        if len(todos_perdedores) > 1:
+            nueva_fase_p = Fase(len(self._bracket_perdedores) + 1)
+            for i in range(0, len(todos_perdedores), 2):
+                if i + 1 < len(todos_perdedores):
+                    nueva_fase_p.partidas.append(Partida(todos_perdedores[i], todos_perdedores[i + 1]))
+            self._bracket_perdedores.append(nueva_fase_p)
+            self._fases.append(nueva_fase_p)
 
--- Eliminaciones/eliminaciondirecta.py
+++ Eliminaciones/eliminaciondirecta.py
@@ -1,8 +1,9 @@
-#Eliminacion directa(si pierdes te eliminas)
+# Eliminacion directa: si pierdes quedas eliminado
 
 from Eliminaciones.bracketeliminacion import BracketEliminacion
 from Principal.fase import Fase
 from Principal.partida import Partida
+
 
 class EliminacionDirecta(BracketEliminacion):
     def gen_bracket(self):
@@ -14,22 +15,3 @@ class EliminacionDirecta(BracketEliminacion):
             fase.partidas.append(partido)
 
         self._fases.append(fase)
-
-    def siguiente_fase(self):
-        fase_actual = self._fases[-1]
-        ganadores = []
-        for partida in fase_actual.partidas:
-            if partida.resultado is None:
-                continue
-            ganador = partida.resultado.ganador()
-            if ganador == 1:
-                ganadores.append(partida.equipo1)
-            else:
-                ganadores.append(partida.equipo2)
-
-        nueva_fase = Fase(fase_actual.numero + 1)
-        for i in range(0, len(ganadores), 2):
-            if i + 1 < len(ganadores):
-                nueva_fase.partidas.append(Partida(ganadores[i], ganadores[i + 1]))
-
-        self._fases.append(nueva_fase)
